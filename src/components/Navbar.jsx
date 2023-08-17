09e6bf063087a15fa0e3ab1019df50eaaf4ab758
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-08-16 19:25:47 -0500

    edited bulma for my navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,33 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
-      <div className="navbar-menu">
+      {<div class="tabs is-small">
+  <ul>
+    <li class="is-active"><a>Home</a></li>
+    <li><a>About Me</a></li>
+    <li><a>Portfolio</a></li>
+    <li><a>Contact</a></li>
+    <li><a>Work</a></li>
+  </ul>
+</div>/* <div className="navbar-menu">
         <div className="navbar-start">
-          <a className="navbar-item button is-info" href="/">
+          <a className="navbar-item " href="/">
             Home
           </a>
-          <a className="navbar-item button is-info" href="/aboutme">
+          <a className="navbar-item " href="/aboutme">
             About Me
           </a>
-          <a className="navbar-item button is-info" href="/portfolio">
+          <a className="navbar-item " href="/portfolio">
             Portfolio
           </a>
-          <a className="navbar-item button is-info" href="/contact">
+          <a className="navbar-item " href="/contact">
             Contact
           </a>
-          <a className="navbar-item button is-info" href="/work">
+          <a className="navbar-item " href="/work">
             Work
           </a>
-        </div>
-      </div>
+        </div> */}
+      
     </nav>
   );
 }
