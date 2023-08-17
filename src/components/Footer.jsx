655439bb66e624838a0cdf5ff04f87e8de398685
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-08-17 02:42:34 -0500

    spotify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,23 +2,24 @@ import React from "react";
 
 function Footer() {
   return (
-    <div id="contact">{
-    <footer className="footer has-background-primary">
-      <div className="content has-text-centered">
-        <div className="columns is-centered">
-          <div className="column is-half">
-            <ul className="footer-links is-flex">
-              <li className="mr-6"><a className="has-text-white button is-info" href="mailto:wontonnoodles23@gmail.com">Email</a></li>
-              <li className="mr-6"><a className="has-text-white button is-info" href="tel:732.309.5817">Phone</a></li>
-              <li className="mr-6"><a className="has-text-white button is-info" href="https://open.spotify.com/playlist/75rMs9goFl18iXSisB3j5v?si=d6c9f05012ce4989">Spotify</a></li>
-              <li className="mr-6"><a className="has-text-white button is-info" href="https://twitter.com/wontonnoodles23">Twitter</a></li>
-              <li><a className="has-text-white button is-info" href="https://github.com/wontonchris">GitHub</a></li>
-            </ul>
+    <div>
+      <nav className="breadcrumb is-centered" aria-label="breadcrumbs">
+        <footer className="footer has-background-primary">
+          <div className="container">
+            <div id="contact" className="hero is-small has-text-centered">
+              <ul className="footer-links is-flex">
+                <li className="mr-6"><a className="has-text-white is-size-5" href="mailto:wontonnoodles23@gmail.com">Email</a></li>
+                <li className="mr-6"><a className="has-text-white is-size-5" href="tel:732.309.5817">Phone</a></li>
+                <li className="mr-6"><a className="has-text-white is-size-5" href="https://open.spotify.com/playlist/75rMs9goFl18iXSisB3j5v?si=d6c9f05012ce4989">Spotify</a></li>
+                <li className="mr-6"><a className="has-text-white is-size-5" href="https://twitter.com/wontonnoodles23">Twitter</a></li>
+                <li><a className="has-text-white is-size-5" href="https://github.com/wontonchris">GitHub</a></li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </div>
-    </footer>
-}</div>
+        </footer>
+      </nav>
+      <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/album/2a5aMQIXlQYWvhqd0umrgu?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
   );
 }
 
